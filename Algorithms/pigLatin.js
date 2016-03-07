6914798bf40743805e6705ffa8d76b2466a54430
usainzg <unaihtc70@gmail.com>
2016-03-07 20:26:22 +0100

    PigLatin algorithm added

---
+++ Algorithms/pigLatin.js
@@ -0,0 +1,19 @@
+function translate(str) {
+
+  var pigLatin = '';
+  var regex = /[aeiou]/gi;
+
+  if (str[0].match(regex)) {
+    pigLatin = str + 'way';
+
+  } else {
+
+
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+
+
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + 'ay';
+  }
+
+  return pigLatin;
+}
