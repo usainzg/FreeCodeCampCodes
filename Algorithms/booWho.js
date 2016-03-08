d6c2e65e70452e1c8305c6f0fc5adcb5e6e35808
usainzg <unaihtc70@gmail.com>
2016-03-08 23:07:07 +0100

    booWho algorithm edited

--- Algorithms/booWho.js
+++ Algorithms/booWho.js
@@ -1,3 +1,9 @@
-function boo(bool) {
+// option 1, better
+(function(bool){
+  return typeof bool === 'boolean';
+})(false);
+
+// option 2
+(function(bool){
   return bool === true ? true : false || bool === false ? true : false;
-}
+})(true);
